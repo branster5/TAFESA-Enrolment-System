3a735bd53f64dc4754bcff8d33148474fd9b8869
branster5 <55789293+branster5@users.noreply.github.com>
2025-09-03 20:04:48 +0930

    Subject testing

--- Models/Subject.cs
+++ Models/Subject.cs
@@ -8,5 +8,27 @@ namespace TAFESA_Enrolment_System.Models
 {
     internal class Subject
     {
+        const string DEF_SUBJECT_CODE = "No subject code provided";
+        const string DEF_SUBJECT_NAME = "No subject name provided";
+        const decimal DEF_COST = 0;
+        public string SubjectCode { get; set; }
+        public string SubjectName { get; set; }
+        public decimal Cost { get; set; }
+
+        // No arg constructor
+        public Subject() : this(DEF_SUBJECT_CODE, DEF_SUBJECT_NAME, DEF_COST) { }
+
+        // All arg constructor
+        public Subject(string subjectCode, string subjectName, decimal cost)
+        {
+            SubjectCode = subjectCode;
+            SubjectName = subjectName;
+            Cost = cost;
+        }
+
+        public override string ToString()
+        {
+            return "SubjectCode: " + SubjectCode + " SubjectName: " + SubjectName + " Cost: " + Cost.ToString();
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TAFESA_Enrolment_System.Models;
+
+namespace TAFESA_Enrolment_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // SUBJECT:
+
+            // Test subject constructors
+            Subject noArgSubject = new Subject();
+            Subject allArgSubject = new Subject("AAA", "Apply Anything Always", 999.99M);
+            Console.WriteLine(noArgSubject);
+            Console.WriteLine(allArgSubject);
+
+            // Test Setters and Getters
+            allArgSubject.SubjectName = "TestName";
+            allArgSubject.SubjectCode = "TestCode";
+            allArgSubject.Cost += 0.01M;
+            Console.WriteLine("After updates:");
+            Console.WriteLine("Code: " + allArgSubject.SubjectCode);
+            Console.WriteLine("Name: " + allArgSubject.SubjectName);
+            Console.WriteLine("Cost: " + allArgSubject.Cost);
+        }
+    }
+}
