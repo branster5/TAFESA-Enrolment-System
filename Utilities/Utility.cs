b35dd9ecaf92dd6e506d8647d05be69a70643c77
branster5 <55789293+branster5@users.noreply.github.com>
2025-10-26 21:32:21 +1030

    Exception handing

--- Utilities/Utility.cs
+++ Utilities/Utility.cs
@@ -26,12 +26,35 @@ namespace TAFESA_Enrolment_System.Utilities
         public static int LinearSearchArray<T>(T[] array, T target) where T:IComparable<T>
         {
             ArgumentNullException.ThrowIfNull(array);
-            for (int i = 0; i < array.Length; i++)
+            try
             {
-                if (array[i].CompareTo(target) == 0)
-                    return i;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i].CompareTo(target) == 0)
+                        return i;
+                }
+                return -1;
             }
-            return -1;
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("LinearSearchArray comparison failed.", ex);
+            }
+        }
+
+        /// <summary>
+        /// Ensures the array is sorted ascending according to T.CompareTo.
+        /// Throws an ArgumentException if any adjacent pair is out of order.
+        /// </summary>
+        private static void EnsureSortedAscending<T>(T[] array) where T : IComparable<T>
+        {
+            if (array.Length < 2) return;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                // Ensure the array is in ascending order
+                if (array[i - 1].CompareTo(array[i]) > 0)
+                    throw new ArgumentException("Array must be sorted in ascending order for BinarySearch.", nameof(array));
+            }
         }
 
         /// <summary>
@@ -54,23 +77,33 @@ namespace TAFESA_Enrolment_System.Utilities
         public static int BinarySearchArray<T>(T[] array, T target) where T : IComparable<T>
         {
             ArgumentNullException.ThrowIfNull(array);
+
+            EnsureSortedAscending(array);
+
             int min = 0;
             int max = array.Length - 1;
             int mid;
             // if do while, breaks on empty arrays
-            while (min <= max)
+            try
             {
-                mid = min + ((max - min) / 2);
-                //possible replace to target.CompareTo(array[mid])
-                int compareValue = array[mid].CompareTo(target);
-                if (compareValue == 0)
-                    return mid;
-                if (compareValue < 0)
-                    min = mid + 1;
-                else
-                    max = mid - 1;
-            } 
-            return -1;  // -1 is returned when not found
+                while (min <= max)
+                {
+                    mid = min + ((max - min) / 2);
+                    //possible replace to target.CompareTo(array[mid])
+                    int compareValue = array[mid].CompareTo(target);
+                    if (compareValue == 0)
+                        return mid;
+                    if (compareValue < 0)
+                        min = mid + 1;
+                    else
+                        max = mid - 1;
+                }
+                return -1;  // -1 is returned when not found
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("BinarySearchArray comparison failed.", ex);
+            }
         }
 
         /// <summary>
